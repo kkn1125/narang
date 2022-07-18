af699cab9e3667ed0ee862c6ad16e3f286931d18
kimson <xpfjrnseks1@naver.com>
2022-07-19 00:08:22 +0900

    [FE][:hammer_and_wrench: FEAT]:  역 스타일 높이 수정 + 인증 페이지 라우터 추가

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -15,6 +15,9 @@ import Recommend from "./pages/Diary/Recommend";
 import Graph from "./pages/Diary/Graph";
 import Share from "./pages/Diary/Share";
 import Usage from "./pages/Diary/Usage";
+import AuthLayout from "./components/templates/AuthLayout";
+import SignIn from "./pages/Auth/SignIn";
+import SignUp from "./pages/Auth/SignUp";
 
 export default function App() {
   const elements = useRoutes([
@@ -24,6 +27,14 @@ export default function App() {
       children: [
         { index: true, element: <Home /> },
         { path: "about", element: <About /> },
+      ],
+    },
+    {
+      path: "/auth",
+      element: <AuthLayout />,
+      children: [
+        { path: "signin", element: <SignIn /> },
+        { path: "signup", element: <SignUp /> },
       ],
     },
     {
