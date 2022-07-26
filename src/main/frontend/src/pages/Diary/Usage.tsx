ff26f7d71ac40dc596d5797f80e49bff90e5057d
kimson <xpfjrnseks1@naver.com>
2022-07-26 19:36:01 +0900

    [FE][:hammer_and_wrench: FEAT]: 일기쓰리 폼데이터 검증 + 서버요청 구문 작성 + 번역,감정분석 클래스 추가

--- src/main/frontend/src/pages/Diary/Usage.tsx
+++ src/main/frontend/src/pages/Diary/Usage.tsx
@@ -1,6 +1,34 @@
-import React from "react";
+import { Button } from "@mui/material";
+import axios from "axios";
+import React, { useEffect } from "react";
+import Sentiment from "sentiment";
 
 function Usage() {
+  useEffect(() => {
+    // 카카오결제
+    // axios({
+    //   method: "post",
+    //   url: url,
+    //   params: {
+    //     cid: "TC0ONETIME",
+    //     partner_order_id: "a",
+    //     partner_user_id: "a",
+    //     item_name: "ohoraming",
+    //     quantity: 1,
+    //     total_amount: 5000,
+    //     tax_free_amount: 10,
+    //     approval_url: "http://localhost:3000",
+    //     cancel_url: "http://localhost:3000",
+    //     fail_url: "http://localhost:3000",
+    //   },
+    //   headers: {
+    //     "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    //     Authorization: "KakaoAK c8a57b4921f2968cebd9f5187277d725",
+    //   },
+    // }).then((res) => {
+    //   console.log(res.data);
+    // });
+  }, []);
   return <div>usage</div>;
 }
 
