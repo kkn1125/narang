94a874f4165180783f04fe19d7700470c3c3ff70
kimson <xpfjrnseks1@naver.com>
2022-07-27 14:56:18 +0900

    [FE][:bulb: ADD]: 푸터 컴포넌트 추가

--- src/main/frontend/src/components/templates/Layout.tsx
+++ src/main/frontend/src/components/templates/Layout.tsx
@@ -1,17 +1,19 @@
 import { Box } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../organisms/Footer";
 import Header from "../organisms/Header";
 
 function Layout() {
-	return (
-		<Box>
-			<Header />
-			<Box>
-				<Outlet />
-			</Box>
-		</Box>
-	);
+  return (
+    <Box>
+      <Header />
+      <Box>
+        <Outlet />
+      </Box>
+      <Footer />
+    </Box>
+  );
 }
 
 export default Layout;
