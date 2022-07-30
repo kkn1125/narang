b19d32c58c9219d0376edfd4454a9b9d3c73c806
kimson <xpfjrnseks1@naver.com>
2022-07-31 01:39:55 +0900

    [BE][:bulb: ADD]: mongoDB 생성,업데이트 시간 자동 할당을 위한 mongoaudit 설정 추가 / #54 참조

--- src/main/java/com/narang/web/config/UserAuditor.java
+++ src/main/java/com/narang/web/config/UserAuditor.java
@@ -1,14 +1,14 @@
 package com.narang.web.config;
 
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component("mongoAuditingConfig")
-public class MongoAuditingConfig implements AuditorAware<String> {
+public class UserAuditor implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("narang");    }
+        return Optional.of("narang");
+    }
 }
