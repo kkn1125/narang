f8ac4c48b66e19f171c268ee159daafea0b2f9f6
kimson <xpfjrnseks1@naver.com>
2022-08-01 19:25:26 +0900

    [FE][:hammer_and_wrench: FEAT]: faceImage api 생성 + user api 수정

---
+++ src/main/frontend/src/apis/faceImage/index.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosResponse } from "axios";
+import FaceImage from "../../models/FaceImage";
+
+const handleReceiveData = (res: AxiosResponse<any, any>): FaceImage | null =>
+  res.data as FaceImage;
+
+const handleReceiveError = (err: { message: any }) => {
+  console.log(err.message);
+};
+
+const addFaceImage = (formData: FormData) => {
+  axios
+    .post("/api/face", formData)
+    .then(handleReceiveData)
+    .catch(handleReceiveError);
+};
+
+const deleteAll = (uid: string) => {
+  axios
+    .delete(`/api/face/${uid}`)
+    .then(handleReceiveData)
+    .catch(handleReceiveError);
+};
+
+const deleteFaceImageById = (uid: string, ids: string[]) => {
+  const faces = new FormData();
+  ids.forEach((id) => {
+    // 서버단에서 String[]이 아닌 List<String>으로 받기 때문에 name을 동일하게 해서 여러 데이터를 넣는다. 그러면 서버단에서 List로 변환
+    faces.append("ids", id);
+  });
+
+  axios
+    .delete(`/api/face/${uid}`, {
+      data: faces,
+    })
+    .then(handleReceiveData)
+    .catch(handleReceiveError);
+};
+
+export { addFaceImage, deleteAll, deleteFaceImageById };
