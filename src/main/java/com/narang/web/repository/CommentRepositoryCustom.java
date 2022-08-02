9799570ac9d48d1b3dbeeb03113ee945b6bc00e1
kimson <xpfjrnseks1@naver.com>
2022-08-02 21:25:44 +0900

    [BE][:wrench: FIX]: comment repository 및 customRepository 생성 + restController 기본 api 작성

--- src/main/java/com/narang/web/repository/CommentRepositoryCustom.java
+++ src/main/java/com/narang/web/repository/CommentRepositoryCustom.java
@@ -1,2 +1,7 @@
-package com.narang.web.repository;public interface CommentRepositoryCustom {
+package com.narang.web.repository;
+
+import com.narang.web.entity.Comment;
+
+public interface CommentRepositoryCustom {
+    public Comment insertComment(Comment comment);
 }
--- src/main/java/com/narang/web/restController/CommentRestController.java
+++ src/main/java/com/narang/web/restController/CommentRestController.java
@@ -3,6 +3,7 @@ package com.narang.web.restController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.narang.web.entity.Comment;
+import com.narang.web.entity.Diary;
 import com.narang.web.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -22,9 +23,14 @@ public class CommentRestController {
         return mapper.writeValueAsString(repository.findAll());
     }
 
-    @RequestMapping(path = "/comment", method = {RequestMethod.POST, RequestMethod.PUT})
-    public Boolean insert(Comment comment, HttpServletRequest request) {
+    @PostMapping("/comment")
+    public String insert(Comment comment) {
         System.out.println(comment);
+        Comment newComment = repository.insertComment(comment);
+        return newComment.getId();
+    }
+    @PutMapping("/comment")
+    public Boolean update(Comment comment) {
         repository.save(comment);
         return true;
     }
