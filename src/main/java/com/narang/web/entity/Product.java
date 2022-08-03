13544e587da893f6877aa7a20c0e5f17b612885e
ohoraming <kminsmile@gmail.com>
2022-08-03 18:30:13 +0900

    [BE][:wrench: FIX:]: Product 모델 클래스의 Date 수정과 관련 Controller 수정

--- src/main/java/com/narang/web/entity/Product.java
+++ src/main/java/com/narang/web/entity/Product.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
@@ -31,9 +33,11 @@ public class Product {
 
     private String seller;
 
-    private Boolean isSoldOut;
+    private Boolean isSoldOut = false;
 
-    private Date regdate;
+    @CreatedDate
+    private LocalDateTime regdate;
 
-    private Date updates;
+    @LastModifiedDate
+    private LocalDateTime updates;
 }
