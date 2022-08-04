797a6538241db7569f56768bae8b1acaca549466
kimson <xpfjrnseks1@naver.com>
2022-08-04 16:02:24 +0900

    [BE][:hammer_and_wrench: FEAT]: mongoAuditor 설정 변경 + application properties jwt 시크릿키 추가 + securityConfiguration 클래스 테스트 및 주석처리

--- src/main/java/com/narang/web/config/MongoAuditConfig.java
+++ src/main/java/com/narang/web/config/MongoAuditConfig.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(considerNestedRepositories = true, basePackages = "com.narang.web.mongoTemplate")
+@EnableMongoRepositories(basePackages = {"com.narang.web.mongoTemplate", "com.narang.web.repository"})
 @EnableMongoAuditing
 public class MongoAuditConfig {
 }
