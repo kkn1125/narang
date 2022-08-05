1038b0b9fdb0e45cdbe12a9094c2b2b32683aafd
kimson <xpfjrnseks1@naver.com>
2022-08-05 17:44:52 +0900

    [FE][:hammer_and_wrench: FEAT]: 메인 페이지 헤더 푸터 수정 + 브랜드 컴포넌트 추가 + 타이틀 shadow추가

---
+++ src/main/frontend/src/components/atoms/Brand.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import BedtimeOutlinedIcon from "@mui/icons-material/BedtimeOutlined";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const BRAND = "NARANG";
+
+export enum Responsive {
+  Mobile = "mobile",
+  Desktop = "desktop",
+}
+
+interface BrandProps {
+  responsive: Responsive;
+}
+
+const responsiveOption = {
+  [Responsive.Desktop]: { xs: "none", md: "flex" },
+  [Responsive.Mobile]: { xs: "flex", md: "none" },
+};
+
+function Brand({ responsive }: BrandProps) {
+  return (
+    <>
+      <BedtimeOutlinedIcon
+        sx={{ display: responsiveOption[responsive], mr: 1 }}
+      />
+      <Typography
+        variant='h6'
+        noWrap
+        component={Link}
+        to='/'
+        sx={{
+          ...(responsive === Responsive.Mobile && {
+            flexGrow: 1,
+          }),
+          mr: 2,
+          display: responsiveOption[responsive],
+          fontFamily: "monospace",
+          fontWeight: 700,
+          letterSpacing: ".3rem",
+          color: "inherit",
+          textDecoration: "none",
+        }}>
+        {BRAND}
+      </Typography>
+    </>
+  );
+}
+
+export default Brand;
